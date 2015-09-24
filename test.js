3434502c19eb4e14cbf37685288ed3676f0355fd
andrepolischuk <andre.polischuk@gmail.com>
2015-09-24 17:35:16 +0300

    Add ava instead of mocha

--- test.js
+++ test.js
@@ -1,38 +1,40 @@
 'use strict';
+var test = require('ava');
 var assert = require('yeoman-generator').assert;
 var helpers = require('yeoman-generator').test;
 var join = require('path').join;
 
-describe('mo', function () {
-  before(function (done) {
-    helpers.run(join(__dirname, './app'))
-      .inDir(join(__dirname, './temp'))
-      .withOptions({'skip-install': true})
-      .withPrompts({
-        name: 'test',
-        description: 'Test',
-        githubUsername: 'andrepolischuk',
-        cli: true
-      })
-      .on('end', done);
-  });
+test.before(function (t) {
+  helpers.run(join(__dirname, './app'))
+    .inDir(join(__dirname, './temp'))
+    .withOptions({'skip-install': true})
+    .withPrompts({
+      name: 'test',
+      description: 'Test',
+      githubUsername: 'andrepolischuk',
+      cli: true
+    })
+    .on('end', function () {
+      t.end();
+    });
+});
 
-  it('should generate files', function () {
-    assert.file([
-      '.editorconfig',
-      '.gitignore',
-      '.npmignore',
-      '.travis.yml',
-      'cli.js',
-      'index.js',
-      'test.js',
-      'package.json',
-      'README.md'
-    ]);
+test('should generate files', function (t) {
+  assert.file([
+    '.editorconfig',
+    '.gitignore',
+    '.npmignore',
+    '.travis.yml',
+    'cli.js',
+    'index.js',
+    'test.js',
+    'package.json',
+    'README.md'
+  ]);
 
-    assert.fileContent('package.json', /"name": "test"/);
-    assert.fileContent('package.json', /"main": "index\.es5\.js"/);
-    assert.fileContent('package.json', /"bin": "cli\.es5\.js"/);
-    assert.fileContent('package.json', /"author": "Andrey Polischuk <andre\.polischuk@gmail\.com> \(https:\/\/twitter\.com\/andrepolischuk\)"/);
-  });
+  assert.fileContent('package.json', /"name": "test"/);
+  assert.fileContent('package.json', /"main": "index\.es5\.js"/);
+  assert.fileContent('package.json', /"bin": "cli\.es5\.js"/);
+  assert.fileContent('package.json', /"author": "Andrey Polischuk <andre\.polischuk@gmail\.com> \(https:\/\/twitter\.com\/andrepolischuk\)"/);
+  t.end();
 });
