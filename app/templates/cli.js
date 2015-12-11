f3362db6820a8c6eea302a0af1a0351997fc930c
andrepolischuk <andre.polischuk@gmail.com>
2015-12-11 18:58:12 +0300

    Simplify cli help

--- app/templates/cli.js
+++ app/templates/cli.js
@@ -2,14 +2,12 @@
 import meow from 'meow';
 import <%= camelName %> from './index';
 
-const cli = meow({
-  help: [
-    'Usage',
-    '  <%= name %> <input>',
-    '',
-    'Example',
-    '  <%= name %> world'
-  ]
-});
+const cli = meow(`
+    Usage
+      <%= name %> <input>
+
+    Example
+      <%= name %> world
+`);
 
 console.log(<%= camelName %>(cli.input[0]));
