970f76a5e5c39f1bf47217dcbe8f7e25fb24f525
andrepolischuk <andre.polischuk@gmail.com>
2015-12-11 18:55:06 +0300

    Fix test import

--- app/templates/test.js
+++ app/templates/test.js
@@ -1,5 +1,5 @@
 import test from 'ava';
-import <%= camelName %> from './index';
+import <%= camelName %> from './index.es5';
 
 test('should return `Hello world`', t => {
   t.true(<%= camelName %>('world') === 'Hello world');
